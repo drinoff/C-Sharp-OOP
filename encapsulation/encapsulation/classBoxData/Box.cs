70ea6cc8e5c89b2ede2a76d730d1dc8fc063f6da
Drinoff <drinoff@gmail.com>
2020-10-27 15:54:29 +0100

    up

---
+++ encapsulation/encapsulation/classBoxData/Box.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+        private double Length
+        {
+            get
+            {
+                return this.length;
+            }
+            set
+            {
+                try
+                {
+                    if (value <= 0)
+                    {
+                        throw new ArgumentException("Length cannot be zero or negative.");
+                    }
+                    this.length = value;
+                }
+                catch (Exception msg)
+                {
+                    Console.WriteLine(msg.Message);
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        private double Width
+        {
+            get => this.width;
+            set
+            {
+                try
+                {
+                    if (value <= 0)
+                    {
+                        throw new ArgumentException("Width cannot be zero or negative.");
+                    }
+                    this.width = value;
+                }
+                catch (Exception msg)
+                {
+                    Console.WriteLine(msg.Message);
+                    Environment.Exit(0);
+                }
+                                
+            }
+        }
+        private double Height
+        {
+            get => this.height;
+            set
+            {
+                try
+                {
+                    if (value <= 0)
+                    {
+                        throw new ArgumentException("Height cannot be zero or negative.");
+                    }
+                    this.height = value;
+                }
+                catch (Exception msg)
+                {
+                    Console.WriteLine(msg.Message);
+                    Environment.Exit(0);
+                }
+            }
+        }
+        public double SurfaceArea() => 2 * this.Length * this.Width + 2 * this.Length * this.Height + 2 * this.Width * this.Height;
+
+        public double LateralSurfaceArea() => 2 * this.Length * this.Height + 2 * this.Width * this.Height;
+
+        public double Volume() => this.Length * this.Width * this.Height;
+    }
+}
---
+++ encapsulation/encapsulation/classBoxData/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace classBoxData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var length = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            var rectangle = new Box(length, width, height);
+
+            Console.WriteLine($"Surface Area - {rectangle.SurfaceArea():F2}");
+            Console.WriteLine($"Lateral Surface Area - {rectangle.LateralSurfaceArea():F2}");
+            Console.WriteLine($"Volume - {rectangle.Volume():F2}");
+            
+        }
+    }
+}
